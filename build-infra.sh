9c15577488c3b87382f4b147399810ffd864bc60
Inacio Klassmann <inaciosk8@gmail.com>
2019-06-08 22:59:05 -0300

    install kubectl on all hosts

--- build-infra.sh
+++ build-infra.sh
@@ -23,13 +23,11 @@ k8s_cluster_master_01=$(terraform output k8s-cluster-master-01)
 k8s_cluster_master_02=$(terraform output k8s-cluster-master-02)
 k8s_cluster_app_01=$(terraform output k8s-cluster-app-01)
 k8s_cluster_app_02=$(terraform output k8s-cluster-app-02)
+AWS_DEFAULT_REGION=$(terraform output AWS_DEFAULT_REGION)
+AWS_DEFAULT_INSTANCE_TYPE=$(terraform output AWS_DEFAULT_INSTANCE_TYPE)
 
-echo "${k8s_cluster_master_01}"
-echo "${k8s_cluster_master_02}"
-echo "${k8s_cluster_app_01}"
-echo "${k8s_cluster_app_02}"
 
-############## Ansible scripts #########
+############## Create ansible inventory #########
 cd ../ansible
 echo "" > hosts
 echo "[k8s-cluster-master-01]" >> hosts
@@ -40,10 +38,34 @@ echo "[k8s-cluster-app-01]" >> hosts
 echo "${k8s_cluster_app_01}" >> hosts
 echo "[k8s-cluster-app-02]" >> hosts
 echo "${k8s_cluster_app_02}" >> hosts
+
+echo "" >> hosts
+echo "[k8s-cluster]" >> hosts
+echo "${k8s_cluster_master_01}" >> hosts
+echo "${k8s_cluster_master_02}" >> hosts
+echo "${k8s_cluster_app_01}" >> hosts
+echo "${k8s_cluster_app_02}" >> hosts
+
+echo "" >> hosts
+echo "[k8s-cluster-master]" >> hosts
+echo "${k8s_cluster_master_01}" >> hosts
+echo "${k8s_cluster_master_02}" >> hosts
+
+echo "" >> hosts
+echo "[k8s-cluster-app]" >> hosts
+echo "${k8s_cluster_app_01}" >> hosts
+echo "${k8s_cluster_app_02}" >> hosts
+
 echo "" >> hosts
 echo "[all:vars]" >> hosts
 echo "ansible_python_interpreter=/usr/bin/python3" >> hosts
 echo "ansible_user=${AWS_USER}" >> hosts
 echo "ansible_ssh_private_key_file=${USER_SSH_PRIVATE_KEY}" >> hosts
+
+############## Running ansible scripts #########
+
+ansible-playbook k8s-cluster.yml -i hosts \
+    --ssh-common-args='-o StrictHostKeyChecking=no' \
+    --extra-vars="AWS_DEFAULT_INSTANCE_TYPE=${AWS_DEFAULT_INSTANCE_TYPE} AWS_ACCESS_KEY=${AWS_ACCESS_KEY} AWS_SECRET_KEY=${AWS_SECRET_KEY} AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}"
 
 exit 0
--- terraform/output.tf
+++ terraform/output.tf
@@ -13,3 +13,11 @@ output "k8s-cluster-app-01" {
 output "k8s-cluster-app-02" {
   value = "${join("\n", aws_instance.k8s-cluster-app-02.*.public_dns)}"
 }
+
+output "AWS_DEFAULT_REGION" {
+  value = "${var.AWS_DEFAULT_REGION}"
+}
+
+output "AWS_DEFAULT_INSTANCE_TYPE" {
+  value = "${var.AWS_DEFAULT_INSTANCE_TYPE}"
+}
